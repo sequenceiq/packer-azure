3a3e4b3d75be13ee468538bf9b50b6a1b4e79d00
lalyos <lalyos@yahoo.com>
2015-10-26 09:13:42 +0100

    Add mocking capability

---
+++ packer/builder/azure/step_create_image_mock.go
@@ -0,0 +1,30 @@
+package azure
+
+import (
+	"github.com/MSOpenTech/packer-azure/packer/builder/azure/constants"
+	"github.com/mitchellh/multistep"
+	"github.com/mitchellh/packer/packer"
+)
+
+type StepCreateImageMock struct {
+	TmpServiceName    string
+	TmpVmName         string
+	UserImageLabel    string
+	UserImageName     string
+	RecommendedVMSize string
+}
+
+func (s *StepCreateImageMock) Run(state multistep.StateBag) multistep.StepAction {
+	ui := state.Get(constants.Ui).(packer.Ui)
+
+	ui.Say("Creating Azure Image. If Succeed This Will Remove the Temorary VM...")
+
+	// CatpureVMImage removes the VM
+	state.Put(constants.ImageCreated, 1)
+	state.Put(constants.VmExists, 0)
+
+	return multistep.ActionContinue
+}
+
+func (s *StepCreateImageMock) Cleanup(state multistep.StateBag) {
+}
